44dc2c9fedfcb07b2594e0816f85eaeb0f8e84be
mattypenny_on_lulu <mattypenny@gmail.com>
2025-06-23 17:52:45 +0100

    Not quite sure what this is

---
+++ functions/Invoke-CopyBsComputerImageToBlog.ps1
@@ -0,0 +1,45 @@
+function Invoke-CopyBsComputerImageToBlog {
+    <#
+.SYNOPSIS
+   xx
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True)][string]$BlogName,
+        [Parameter(Mandatory = $True)][string]$PhotoFolder,
+        [Parameter(Mandatory = $True)][string]$PhotoName
+    )
+   
+    $DebugPreference = $PSCmdlet.GetVariableValue('DebugPreference')
+   
+    write-startfunction
+   
+    [string]$BlogToken = $(Get-BsParameter -parameter BsTestBlogToken)
+    [string]$BlogConfigUri = $(Get-BsParameter -parameter BsTestBlogConfigUri)
+      
+    $BlogConfig = Get-BsBlogConfig -BlogConfigUri $BlogConfigUri -BlogToken $BlogToken
+
+    $Photos = Get-ChildItem -Path $PhotoFolder -Filter "*$PhotoName*"
+        
+    $URLs = foreach ($Photo in $Photos) {
+        $Image = $Photo.FullName
+        write-dbg "`$Image: <$Image>"
+        $headers = @{
+            "Authorization" = "Bearer $BlogToken"
+        }
+        $Params = @{
+            RestMethodHeaders = $headers
+            ImagePath         = $Image
+            BlogConfig        = $BlogConfig
+            BlogName          = $BlogName
+        }
+        $BlogIMageURl = Copy-BsComputerImageToBlog @Params
+        write-dbg "`$BlogIMageURl: <$BlogIMageURl>"
+    }
+   
+    write-endfunction
+    return $URLS
+   
+   
+}
+set-alias post-bsphoto Invoke-CopyBsComputerImageToBlog
